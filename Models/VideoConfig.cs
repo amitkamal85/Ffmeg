d7c5771883b3d458b55a951c738acf7d2c2f7a7b
Amit <amitkamal85@gmail.com>
2024-01-15 17:57:39 +0530

    Initial commit

---
+++ Models/VideoConfig.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NotFinal.Models
+{
+    public partial class VideoConfig
+    {
+        public int Id { get; set; }
+        public string? OriginalVideo { get; set; }
+        public string? Title { get; set; }
+        public string? Sypnosis { get; set; }
+        public string? LowQuality { get; set; }
+        public string? MediumQuality { get; set; }
+        public string? HighQuality { get; set; }
+        public string? VeryHighQuality { get; set; }
+        public string? CreatedBy { get; set; }
+        public string? Duration { get; set; }
+        public DateTime? CreatedDate { get; set; }
+        public string? ProcessingTme { get; set; }
+    }
+}
